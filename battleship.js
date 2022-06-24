09baa73c2b5731b041d92d2be01e4eb9b54a41e0
Alfredo Valle <phredvalle@gmail.com>
2022-06-23 22:36:34 -0500

    base logic for hit/miss

---
+++ battleship.js
@@ -0,0 +1,38 @@
+alert(`Let's battle!`);
+
+let location1 = 3;
+let location2 = 4;
+let location3 = 5;
+let guess;
+let hits = 0;
+let guesses = 0;
+let isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready? Enter # from 1-6");
+    
+    if (guess< 0 || guess > 6) {
+        alert("Enter # from 1-6")
+    } else {
+        guesses++;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!");
+            hits++;
+            if (hits==3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+                
+            }
+ 
+        } else {
+            alert("MISS!");
+        }
+    }    
+    
+}
+
+let stats = "You took " + guesses + "guesses to sink the battleship, " +
+                "which means your shooting accuracy was " + (3/guesses)*100 + "%";
+    
+alert(stats);
